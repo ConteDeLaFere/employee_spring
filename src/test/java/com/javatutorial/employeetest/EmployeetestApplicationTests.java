5a935b3d9c10d739f283157f96ade01409c53b0a
Nikita Malinkin <nik.malina20@gmail.com>
2024-04-01 17:57:49 +0300

    Initial commit

---
+++ src/test/java/com/javatutorial/employeetest/EmployeetestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javatutorial.employeetest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeetestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
